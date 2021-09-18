24c59a5c3a8ea7e0c4ead62f0b04cdf3570f82d3
Vidit S Shah <shah444@purdue.edu>
2021-09-18 12:27:24 -0400

    Refactored html css into Angular

--- meliora-frontend/src/app/app-routing.module.ts
+++ meliora-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'create-post', component: CreatePostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- meliora-frontend/src/app/app.module.ts
+++ meliora-frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingNavbarComponent } from './landing-navbar/landing-navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LandingNavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    HomepageComponent,
+    HomeNavbarComponent,
+    ProfileComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
