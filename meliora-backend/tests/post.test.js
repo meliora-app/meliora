51c0102cf2e51f2c09a1d07fb3fb04a0356ecdcf
Xavier Madera <xmadera44@gmail.com>
2021-09-20 08:59:14 -0500

    Fix to Unit Test Teardown func, test and start script

--- meliora-backend/tests/post.test.js
+++ meliora-backend/tests/post.test.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime/runtime';
 
 import { disconnect } from 'mongoose';
-import { request } from 'supertest';
+import request from 'supertest';
 
 import { Post } from '../src/models/Post.js';
 import { server } from '../src/server.js';
@@ -17,15 +17,29 @@ describe('Unit Tests for Post Router:', () => {
 			.post(`/api/posts/create`)
 			.set('Content-Type', 'application/json')
 			.send({
-				title: 'Hi',
-				content: 'Hi'
+				title: 'Test1 Title',
+				content: 'Test1 Content'
 			});
 		
 		expect(res.status).toBe(400);
 	});
 
-	afterAll(async (done) => {
-		await disconnect();
+	test('Create: Server should return a 200 when a post is made with correct format', async () => {
+		let res = await request(server)
+			.post(`api/posts/create`)
+			.set('Content-Type', 'application/json')
+			.send({
+				title: 'Test2 Title',
+				content: 'Test2 Content',
+				author: process.env.TEST_USER_ID
+			});
+
+		expect(res.status).toBe(200);
+	});
+
+	afterAll(done => {
+		Post.deleteMany({});
+		disconnect();
 
 		done();
 	})
