a997b433227f8f024cedd645ac63889f580f4716
Xavier Madera <xmadera44@gmail.com>
2021-09-20 09:31:53 -0500

    Wrote unit tests for US-19

--- meliora-backend/tests/post.test.js
+++ meliora-backend/tests/post.test.js
@@ -8,6 +8,8 @@ import { server } from '../src/server.js';
 import { connectToDB } from '../src/util/db.js';
 
 describe('Unit Tests for Post Router:', () => {
+	let testPostID;
+
 	beforeAll(async () => {
 		await connectToDB(process.env.TEST_DB_STRING);
 	});
@@ -32,6 +34,36 @@ describe('Unit Tests for Post Router:', () => {
 				title: 'Test2 Title',
 				content: 'Test2 Content',
 				author: process.env.TEST_USER_ID
+			});
+
+		let status = res.status;
+
+		res = await res.json();
+
+		testPostID = res._id;
+
+		expect(status).toBe(200);
+	});
+
+	test('Flag: Server should return a 400 when trying to flag a post that doesn\'t exist', async () => {
+		let res = await request(server)
+			.patch(`api/posts/flag`)
+			.set('Content-Type', 'application/json')
+			.send({
+				post: 'bogus',
+				flagger: process.env.TEST_USER_ID_2,
+			});
+
+		expect(res.status).toBe(400);
+	});
+
+	test('Flag: Server should return a 200 when flagging a post that exists', async () => {
+		let res = await request(server)
+			.patch(`api/posts/flag`)
+			.set('Content-Type', 'application/json')
+			.send({
+				post: testPostID,
+				flagger: process.env.TEST_USER_ID_2,
 			});
 
 		expect(res.status).toBe(200);
