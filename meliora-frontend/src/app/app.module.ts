5b6fb60224240ea37c4d338eca338de898a8ef80
Vidit S Shah <shah444@purdue.edu>
2021-09-26 21:26:12 -0400

    Linked firebase auth with UI

--- meliora-frontend/src/app/app.module.ts
+++ meliora-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,6 +45,7 @@ import { SettingsComponent } from './settings/settings.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
