8d211a858dea5d89218aa8968cb472a52e856f7f
Dithi Saxena <saxena52@purdue.edu>
2021-10-02 01:49:19 -0400

    Fixed settings styling

--- meliora-frontend/src/app/settings/settings.component.ts
+++ meliora-frontend/src/app/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/services/auth.service'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -13,6 +14,31 @@ export class SettingsComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('Inside submit form')
+    this.darkModeCheck(form.value.darkmode);
+  }
+
+  async darkModeCheck(darkmode: string) {
+    let res = await fetch('https://meliora-backend.herokuapp.com/api/users/signup', {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        darkmode
+      })
+    });
+
+    if (res.status == 200) {
+      let resBody = await res.json();
+      localStorage.setItem('darkModeStatus', 'true');
+    } else {
+      let resBody = await res.json();
+      console.log(resBody);
+    }
   }
 
   changePasswordEmail(): void {
