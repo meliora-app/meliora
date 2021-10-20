375720a9168fad1d2a3f49dde3fb3ba9c6d1b5a3
Dithi Saxena <saxena52@purdue.edu>
2021-10-19 23:52:17 -0400

    US-8 Implemented word count display for create post

--- meliora-frontend/src/app/create-post/create-post.component.ts
+++ meliora-frontend/src/app/create-post/create-post.component.ts
@@ -3,17 +3,20 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PostService } from '../shared/services/post.service';
 import { ToastService } from '../shared/services/toast.service';
+import * as angular from "angular";
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css'],
 })
+
 export class CreatePostComponent implements OnInit {
   darkModeStatus = localStorage.getItem("darkModeStatus") == 'true';
   visibilityClicked: boolean = false;
   imageClicked: boolean = false;
-  commentClicked: boolean = false;
+  commentClicked: boolean = false;  
+  words: number = 0;
 
   constructor(
     private postService: PostService,
@@ -25,6 +28,10 @@ export class CreatePostComponent implements OnInit {
     console.log(localStorage.getItem('userID'));
   }
 
+  wordCount(event) {
+    this.words = event.target.value ? event.target.value.split(/\s+/).length : 0;
+  }
+  
   async onSubmitPost(form: NgForm) {
     await this.postService.createPost(
       form.value.title,
