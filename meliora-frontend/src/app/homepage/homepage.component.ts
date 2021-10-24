124a7c57695fd5aecd4169db0d9930e995956a62
Vidit S Shah <shah444@purdue.edu>
2021-10-24 19:25:56 -0400

    minor changes

--- meliora-frontend/src/app/homepage/homepage.component.ts
+++ meliora-frontend/src/app/homepage/homepage.component.ts
@@ -27,25 +27,41 @@ export class HomepageComponent implements OnInit {
   }
 
   async loadFeed() {
-    let res = await fetch('https://meliora-backend.herokuapp.com/api/posts/getAll', {
-      method: "GET"
-    });
+    let res = await fetch(
+      'https://meliora-backend.herokuapp.com/api/posts/getAll',
+      {
+        method: 'GET',
+      }
+    );
     if (res.status == 200) {
       let resBody = await res.json();
       for (let i = 0; i < resBody.length; i++) {
         // need to get user name with get user, would be helpful to include username with post fetch
-        let resUser = await fetch('https://meliora-backend.herokuapp.com/api/users/getUser', {
-          method: "PUT",
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            _id: resBody[i].author
-          })
-        });
+        let resUser = await fetch(
+          'https://meliora-backend.herokuapp.com/api/users/getUser',
+          {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              _id: resBody[i].author,
+            }),
+          }
+        );
+        console.log(resUser.status);
         if (resUser.status == 200) {
           let resUserBody = await resUser.json();
-          this.posts.push(new Post(resBody[i]._id, resBody[i].title, resBody[i].content, resBody[i].author, resBody[i].anonymous, resUserBody.username));
+          this.posts.push(
+            new Post(
+              resBody[i]._id,
+              resBody[i].title,
+              resBody[i].content,
+              resBody[i].author,
+              resBody[i].anonymous,
+              resUserBody.username
+            )
+          );
         }
       }
     }
