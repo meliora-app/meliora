9dc86415106c123a76571ac11443649e6b69c4a5
Vidit S Shah <shah444@purdue.edu>
2021-10-23 22:10:59 -0400

    working on trending categories

--- meliora-backend/src/models/User.js
+++ meliora-backend/src/models/User.js
@@ -1,63 +1,63 @@
-import mongoose from 'mongoose';
-import { PostSchema } from './Post.js';
+import mongoose from "mongoose";
+import { PostSchema } from "./Post.js";
 
 const { SchemaTypes, Schema, model } = mongoose;
 
 const UserSchema = new Schema({
-	username: {
-		type: SchemaTypes.String,
-		required: true
-	},
-	name: {
-		type: SchemaTypes.String,
-		default: '',
-	},
-	phone: {
-		type: SchemaTypes.String,
-		default: '',
-	},
-	sex: {
-		type: SchemaTypes.String,
-		default: '',
-	},
-	email: {
-		type: SchemaTypes.String,
-		required: true
-	},
-	dateOfBirth: {
-		type: SchemaTypes.String,
-		default: '',
-	},
-	password: {
-		type: SchemaTypes.String,
-		required: true
-	},
-	authorList: {
-		type: [SchemaTypes.ObjectId],
-		default: [],
-	},
-	blocked: {
-		type: [SchemaTypes.ObjectId],
-		default: [],
-	},
-	private: {
-		type: SchemaTypes.Boolean,
-		default: false,
-	},
-	bookmarks: {
-		type: [PostSchema],
-		default: []	
-	},
-	bio: {
-		type: SchemaTypes.String,
-		default: "",
-	},
-	darkModeStatus: {
-		type: SchemaTypes.Boolean,
-		default: false
-	}
+  username: {
+    type: SchemaTypes.String,
+    required: true,
+  },
+  name: {
+    type: SchemaTypes.String,
+    default: "",
+  },
+  phone: {
+    type: SchemaTypes.String,
+    default: "",
+  },
+  sex: {
+    type: SchemaTypes.String,
+    default: "",
+  },
+  email: {
+    type: SchemaTypes.String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: SchemaTypes.String,
+    default: "",
+  },
+  password: {
+    type: SchemaTypes.String,
+    required: true,
+  },
+  authorList: {
+    type: [SchemaTypes.ObjectId],
+    default: [],
+  },
+  blocked: {
+    type: [SchemaTypes.ObjectId],
+    default: [],
+  },
+  private: {
+    type: SchemaTypes.Boolean,
+    default: false,
+  },
+  bookmarks: {
+    type: [PostSchema],
+    default: [],
+  },
+  bio: {
+    type: SchemaTypes.String,
+    default: "",
+  },
+  darkModeStatus: {
+    type: SchemaTypes.Boolean,
+    default: false,
+  },
 });
 
-const User = new model('User', UserSchema);
+const User = new model("User", UserSchema);
 
 export { User, UserSchema };
---
+++ meliora-backend/src/routers/categories.js
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { Category } from "../models/Category.js";
+import { Post } from "../models/Post.js";
+
+const catRouter = Router();
+
+catRouter.post("/create", async (req, res) => {
+  var category = req.body;
+  if (!isValidCategory(category)) {
+    res.status(400).send("The object structure of this category is invalid!");
+    return;
+  }
+
+  var categoryDoc;
+  try {
+    categoryDoc = await new Category(category).save();
+  } catch (err) {
+    res.status(500).send(`Backend error`);
+    console.log(err);
+    return;
+  }
+
+  res.status(200).send("Category created successfully!");
+});
+
+catRouter.get("/getAll", (req, res) => {
+  Category.find({})
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("Backend err");
+    });
+});
+
+catRouter.get("/getTrending", async (req, res) => {
+  const categories = await Category.find({});
+
+  for (var i = 0; i < categories.length; i++) {
+    var posts = categories[i]["posts"];
+    var post = await Post.findById(posts[0]);
+    console.log(post);
+  }
+});
+
+//helpers
+const isValidCategory = (category) => {
+  return "name" in category && "description" in category;
+};
+
+export { catRouter };
